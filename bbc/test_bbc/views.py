3e7b7228f45971d09d9afecf5e1ee8b46e134572
darkone0112 <darkone0112@gmail.com>
2023-09-01 13:20:20 +0200

    Adding Https response to handle the stream of data

--- bbc/test_bbc/views.py
+++ bbc/test_bbc/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import FileResponse, HttpResponse
 import json
 import mimetypes
 
@@ -11,12 +12,20 @@ def list_movies(request):
     return render(request, 'index.html', {'movies': movies})
 
 def play_movie(request, movie_title):
-    with open('test_bbc/json/movies.json', 'r') as f:
+    # Assuming you have a JSON file that maps movie titles to file paths
+    with open('../../json/movies.json', 'r') as f:
         movies = json.load(f)
-    movie_to_play = next((movie for movie in movies if movie['title'] == movie_title), None)
-    if movie_to_play:
-        file_path = movie_to_play['file_path']
-        mime_type, encoding = mimetypes.guess_type(file_path)
-        return render(request, 'play_movie.html', {'file_path': file_path, 'mime_type': mime_type})
-    else:
-        return render(request, 'error.html', {'message': 'Movie not found'})
+
+    # Find the correct movie path
+    movie_path = None
+    for movie in movies:
+        if movie['title'] == movie_title:
+            movie_path = movie['file_path']
+            break
+
+    # If movie was not found, return a 404 response
+    if movie_path is None:
+        return HttpResponse('Movie not found', status=404)
+
+    # Return a FileResponse to stream the movie file
+    return FileResponse(open(movie_path, 'rb'))
