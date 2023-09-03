06fa6b194b1e28ff0d7958bf328a62a64ec9c0d1
darkone0112 <darkone0112@gmail.com>
2023-09-03 06:22:48 +0200

    test

--- bbc/test_bbc/views.py
+++ bbc/test_bbc/views.py
@@ -19,44 +19,36 @@ def list_movies(request):
 logger = logging.getLogger(__name__)
 
 def play_movie(request, movie_title):
-    logger.info(f"Received request to play movie: {movie_title}")
+    try:
+        logger.info(f"Received request to play movie: {movie_title}")
 
-    # Assuming you have a JSON file that maps movie titles to file paths
-    with open('/home/darkone0112/bolshoi-burglars-cinema/bbc/test_bbc/json/movies.json', 'r') as f:
-        movies = json.load(f)
+        with open('/home/darkone0112/bolshoi-burglars-cinema/bbc/test_bbc/json/movies.json', 'r') as f:
+            movies = json.load(f)
 
-    # Find the correct movie path
-    movie_path = None
-    for movie in movies:
-        if movie['title'] == movie_title:
-            movie_path = movie['file_path']
-            break
+        movie_path = next((movie['file_path'] for movie in movies if movie['title'] == movie_title), None)
 
-    # If movie was not found, return a 404 response
-    if movie_path is None:
-        logger.error(f"Movie {movie_title} not found.")
-        return HttpResponse('Movie not found', status=404)
+        if movie_path is None:
+            logger.error(f"Movie {movie_title} not found.")
+            return HttpResponse('Movie not found', status=404)
 
-    file_size = os.path.getsize(movie_path)
-    logger.info(f"File size: {file_size}")
+        file_size = os.path.getsize(movie_path)
+        logger.info(f"File size: {file_size}")
 
-    start_range = 0
-    end_range = file_size - 1
-    range_header = request.META.get('HTTP_RANGE', '').strip()
-    range_match = re.match(r'bytes=(\d+)-(\d+)?', range_header)
+        start_range = 0
+        end_range = file_size - 1
+        range_header = request.META.get('HTTP_RANGE', '').strip()
+        range_match = re.match(r'bytes=(\d+)-(\d+)?', range_header)
 
-    if range_match:
-        start_group, end_group = range_match.groups()
-        if start_group is not None:
-            start_range = int(start_group)
-        if end_group is not None:
-            end_range = int(end_group)
+        if range_match:
+            start_group, end_group = range_match.groups()
+            start_range = int(start_group) if start_group else start_range
+            end_range = int(end_group) if end_group else end_range
 
-    logger.info(f"Start range: {start_range}, End range: {end_range}")
+        logger.info(f"Start range: {start_range}, End range: {end_range}")
 
-    with open(movie_path, 'rb') as f:
+        file_obj = open(movie_path, 'rb')
         response = StreamingHttpResponse(
-            FileWrapper(f, blksize=65536 ),
+            FileWrapper(file_obj, blksize=65536),
             status=206 if range_header else 200,
             content_type='video/mp4'
         )
@@ -64,6 +56,10 @@ def play_movie(request, movie_title):
         response['Content-Range'] = f"bytes {start_range}-{end_range}/{file_size}"
         response['Accept-Ranges'] = 'bytes'
 
-    logger.info(f"Streaming initiated for {movie_title}")
+        logger.info(f"Streaming initiated for {movie_title}")
 
-    return response
+        return response
+    except Exception as e:
+        logger.error(f"An error occurred while streaming the movie: {str(e)}")
+        return HttpResponse('Internal Server Error', status=500)
+
