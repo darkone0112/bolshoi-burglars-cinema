1665c70c748835c441213a47023f2b7738c6ede3
darkone0112 <darkone0112@gmail.com>
2023-09-03 05:50:16 +0200

    loggers added to the settings for django

--- bbc/bbc/settings.py
+++ bbc/bbc/settings.py
@@ -26,6 +26,25 @@ SECRET_KEY = 'django-insecure-#s93gr6m2ld7*dgnx=a6o8bd4si64k##vf$z+*9t1ztm*guz^c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 ALLOWED_HOSTS = ['192.168.1.205']
 
 
