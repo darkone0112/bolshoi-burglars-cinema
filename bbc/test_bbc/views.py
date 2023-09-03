4719961979f8891aa922d315894b13024f7de90c
darkone0112 <darkone0112@gmail.com>
2023-09-03 07:44:53 +0200

    iris

--- bbc/test_bbc/views.py
+++ bbc/test_bbc/views.py
@@ -45,6 +45,16 @@ import logging
 # Initialize logging
 logger = logging.getLogger(__name__)
 
+def file_iterator(file_path, start_byte, end_byte, chunk_size=8192):
+    with open(file_path, 'rb') as f:
+        f.seek(start_byte)
+        while start_byte <= end_byte:
+            data = f.read(min(chunk_size, end_byte - start_byte + 1))
+            if not data:
+                break
+            start_byte += len(data)
+            yield data
+
 def play_movie(request, movie_title):
     try:
         logger.info(f"Received request to play movie: {movie_title}")
@@ -61,14 +71,33 @@ def play_movie(request, movie_title):
         file_size = os.path.getsize(movie_path)
         logger.info(f"File size: {file_size}")
 
-        response = FileResponse(open(movie_path, 'rb'), content_type='video/mp4')
+        start_byte = 0
+        end_byte = file_size - 1
 
-        # Explicitly allow range requests
-        #response['Accept-Ranges'] = 'bytes'
+        if 'HTTP_RANGE' in request.META:
+            range_header = request.META['HTTP_RANGE'].split("bytes=")[1]
+            start_byte, end_byte_str = map(str, range_header.split("-"))
+            start_byte = int(start_byte)
+            end_byte = int(end_byte_str) if end_byte_str else file_size - 1
+            end_byte = min(end_byte, file_size - 1)
+
+            response = StreamingHttpResponse(
+                file_iterator(movie_path, start_byte, end_byte),
+                status=206,
+                content_type='video/mp4'
+            )
+            response['Content-Range'] = f"bytes {start_byte}-{end_byte}/{file_size}"
+        else:
+            response = StreamingHttpResponse(
+                file_iterator(movie_path, start_byte, end_byte),
+                content_type='video/mp4'
+            )
+
+        response['Content-Length'] = end_byte - start_byte + 1
+        response['Accept-Ranges'] = 'bytes'
 
         logger.info(f"Streaming initiated for {movie_title}")
 
-        # Let's log more information for debugging
         range_header = request.META.get('HTTP_RANGE', '').strip()
         range_match = re.match(r'bytes=(\d+)-(\d+)?', range_header)
         if range_match:
